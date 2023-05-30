54937cc8a412a16744f05d02984fdae6c8afb4cd
Nyx <tessasie15@gmail.com>
2023-05-30 14:56:25 +0800

    Added content into the redis.go file, and added an error message in main.go in case of redis server failure.

--- rpc-server/redis.go
+++ rpc-server/redis.go
@@ -1,1 +1,26 @@
 package main
+
+import (
+	"context"
+	"github.com/redis/go-redis/v9"
+)
+
+type RedisClient struct {
+	cli *redis.Client
+}
+
+func (c *RedisClient) InitClient(ctx context.Context, address, password string) error {
+	r := redis.NewClient(&redis.Options{
+		Addr:     address,
+		Password: password, // no password set
+		DB:       0,        // use default DB
+	})
+
+	// test connection
+	if err := r.Ping(ctx).Err(); err != nil {
+		return err
+	}
+
+	c.cli = r
+	return nil
+}
