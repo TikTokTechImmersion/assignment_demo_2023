a3e50971a84f6b60003df2b801f16f2243e2cae5
cheeheng <ch.tan020@gmail.com>
2023-06-12 00:47:42 +0800

    Fix edge case where : messages up with chat parameter syntax

--- http-server/main.go
+++ http-server/main.go
@@ -55,6 +55,11 @@ func sendMessage(ctx context.Context, c *app.RequestContext) {
 	sender := c.Query("sender")
 	receiver := c.Query("receiver")
 
+	if strings.Count(sender, ":") >= 1 || strings.Count(receiver, ":") >= 1 {
+		c.String(consts.StatusBadRequest, "Sender name and receiver name cannot contain the character :")
+		return
+	}
+
 	if strings.Compare(sender, receiver) < 0 {
 		req.Chat = sender + ":" + receiver
 	} else {
